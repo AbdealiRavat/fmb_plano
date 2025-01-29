52913eb999eceeabc696636a9cc12cad063a1382
Abdeali Ravat <rawatabdeali22@gmail.com>
2025-01-29 17:52:59 +0530

    firebase options file added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fmb_app/firebase_options.dart';
 import 'package:fmb_app/routes/app_routes.dart';
 import 'package:fmb_app/routes/routes_name.dart';
 import 'package:fmb_app/util/constant.dart';
@@ -10,12 +11,7 @@ import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyDM8J5MyZGydxG8TZhlHWUzRaEIC6PjL54",
-          appId: "1:45099362023:android:d9cff37fd8dc74f414a442",
-          messagingSenderId: "45099362023",
-          projectId: "fmb-singapore"));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   FirebaseMessaging.onBackgroundMessage(bgHandler);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
